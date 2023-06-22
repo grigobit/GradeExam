8f30192ded7e682491c3a84d44207e94b57489e0
grigobit <grigoriou.christos@gmail.com>
2023-06-22 09:04:34 +0200

    Answer of question 8.5.1

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
                 {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
 
         char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+        int[] highestcount = new int[10];
 
         for (int row = 0; row < studentAnswers.length; row++) {
             int countCorrectAnswers = 0;
@@ -19,7 +20,21 @@ public class Main {
                 if (studentAnswers[row][column] == keys[column])
                     countCorrectAnswers += 1;
             }
+            highestcount[row] = countCorrectAnswers;
             System.out.println("Student " + (row + 1) + " answered: " + countCorrectAnswers + " correct questions");
         }
+
+        //How do you modify the code so it also displays the highest count and the student
+        //with the highest count?
+        int maxCorrectAnswers = highestcount[0];
+        int studentID = 0;
+        for (int i = 0; i < highestcount.length; i++) {
+            if (highestcount[i] > maxCorrectAnswers) {
+                maxCorrectAnswers = highestcount[i];
+                studentID = i;
+            }
+        }
+        System.out.println("\nStudent " + (studentID+1) + " has the max corrects answers: " + maxCorrectAnswers );
     }
+
 }
