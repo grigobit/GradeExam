b119b414e8c4dd0547bf2556a10f73e652b90544
grigobit <grigoriou.christos@gmail.com>
2023-06-22 08:44:09 +0200

    Case Study: Grading a Multiple-Choice Test

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char[][] studentAnswers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+        for (int row = 0; row < studentAnswers.length; row++) {
+            int countCorrectAnswers = 0;
+            for (int column = 0; column < studentAnswers[row].length; column++) {
+                if (studentAnswers[row][column] == keys[column])
+                    countCorrectAnswers += 1;
+            }
+            System.out.println("Student " + (row + 1) + " answered: " + countCorrectAnswers + " correct questions");
+        }
+    }
+}
